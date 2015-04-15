9f33cd80bd17a4e513c427d27448f3331b3c47d5
Dave1840438 <david.sylvestre@sympatico.ca>
2015-04-15 08:52:54 -0400

    commencer la background task

--- TP1-ASP/Global.asax.cs
+++ TP1-ASP/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.UI;
@@ -19,6 +20,32 @@ namespace TP1_ASP
             Application["SessionTimeout"] = 5.0;
 
             Application["OnlineUsers"] = new List<long>();
+            AddTask("DoStuff", 60);
+        }
+
+        private static CacheItemRemovedCallback OnCacheRemove = null;
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+
+        private void AddTask(string name, int seconds)
+        {
+           OnCacheRemove = new CacheItemRemovedCallback(CacheItemRemoved);
+           HttpRuntime.Cache.Insert(name, seconds, null,
+               DateTime.Now.AddSeconds(seconds), Cache.NoSlidingExpiration,
+               CacheItemPriority.NotRemovable, OnCacheRemove);
+        }
+
+        public void CacheItemRemoved(string k, object v, CacheItemRemovedReason r)
+        {
+           if (k == "DoStuff")
+           {
+
+           }
+           // do stuff here if it matches our taskname, like WebRequest
+           // re-add our task so it recurs
+           AddTask(k, Convert.ToInt32(v));
         }
 
         protected void Session_Start(object sender, EventArgs e)
